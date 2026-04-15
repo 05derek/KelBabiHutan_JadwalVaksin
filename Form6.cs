35feb56b67b4ff608bfad3739e051bd8a6473076
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2026-04-15 19:37:59 +0700

    coding an load data form 6

--- Form6.cs
+++ Form6.cs
@@ -40,9 +40,35 @@ namespace WindowsFormsApp1
         }
         private void LoadData()
         {
-           
-            
-        }
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    // Query untuk mengambil detail booking beserta info jadwalnya
+                    string query = @"
+                        SELECT b.booking_id, b.user_id, b.jadwal_id, j.tanggal, j.waktu 
+                        FROM Booking b
+                        LEFT JOIN Jadwal j ON b.jadwal_id = j.jadwal_id
+                        WHERE b.booking_id = @Bid";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Bid", bookingId);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Isi TextBox berdasarkan data dari database
+                            textBox1.Text = reader["booking_id"].ToString();
+                            textBox2.Text = reader["user_id"].ToString();
+                            textBox3.Text = reader["jadwal_id"].ToString();
+
+                            // Jika kamu punya label tambahan untuk info tanggal/waktu
+                            // lblInfo.Text = $"Jadwal saat ini: {reader["tanggal"]} jam {reader["waktu"]}";
+                        }
+
+                    }
 
 
         private void button1_Click(object sender, EventArgs e)
