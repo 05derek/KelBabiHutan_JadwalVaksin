55b96833b3592b0cf7032c3e585f21a1887dc199
Derek Cadudasa <derek.dzakir.ft24@mail.umy.ac.id>
2026-06-25 04:10:27 +0700

    add class session

---
+++ Session.cs
@@ -0,0 +1,9 @@
+﻿namespace UCP_1_Revisi
+{
+    public static class Session
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+    }
+}
