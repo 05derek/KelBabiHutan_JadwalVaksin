4f33a5cf1f3a5c689118e27c1efd342edf07df43
Derek Cadudasa <derek.dzakir.ft24@mail.umy.ac.id>
2026-06-25 04:09:40 +0700

    add class program

--- Program.cs
+++ Program.cs
@@ -1,28 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("windows")]
 
 namespace UCP_1_Revisi
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            // Pilih form mana yang ingin dijalankan pertama kali saat aplikasi dibuka
-            // Jika Form revisi kamu bernama FormAwal:
+            ApplicationConfiguration.Initialize();
             Application.Run(new FormAwal());
-
-            // Atau jika ingin tetap menggunakan Form4 dari repo lama:
-            // Application.Run(new Form4());
         }
     }
 }
